9fb7de3dc3d0c44dea97e16d75ac386b55f8d803
Stephen L Belden <belden.stephen@gmail.com>
2015-10-20 20:58:43 -0600

    We're packing bins now!

---
+++ BinPacking/BinPacking/Main.cpp
@@ -0,0 +1,16 @@
+// Stephen Belden
+
+// 2015-Oct-20
+
+#include <iostream>
+
+int main() {
+	std::cout << "This is my bin:" << std::endl << std::endl
+		<< " ____#____" << std::endl;
+	for(int i = 0; i < 7; i++) {
+		std::cout << " |       |" << std::endl;
+	}
+	std::cout << " ---------" << std::endl
+		<< std::endl << "There are many like it, but mine is well-packed."
+		<< std::endl << std::endl;
+}
