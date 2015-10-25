2ca058f558e7fb8df636e53c00c0afb42e3a61b5
Stephen L Belden <belden.stephen@gmail.com>
2015-10-25 15:41:19 -0600

    Working command options, function stubs

--- BinPacking/BinPacking/Main.cpp
+++ BinPacking/BinPacking/Main.cpp
@@ -26,31 +26,35 @@ int main() {
 	cout << " ---------" << endl
 		<< endl << "There are many like it, but mine is well-packed."
 		<< endl << endl;
-	while(1) {
-		string command;
-		unsigned int toGenerate = 0;
-		cout << "Enter a command (tests, random, exit): ";
-		char commandLetter;
-		cin >> commandLetter;
-		switch(commandLetter) {
-		case 't':
-			cout << "This would run the tests if the tests existed."
-				<< endl;
-			break;
-		case 'r':
-			cout << "Generate how many random objects? ";
-			cin >> toGenerate;
-			generateRandom(toGenerate);
-			break;
-		case 'e': exit(0);
-		default:
-			cout << "Unrecognized command." << endl;
-		};
-		cout << endl << endl;
+
+	string command;
+	unsigned int toGenerate = 0;
+	cout << "Enter a command (tests, random): ";
+	getline(cin, command);
+	if(command.substr(0, 1) == string("t")) {
+		cout << "This would run the tests if the tests existed." << endl;
 	}
+	else if(command.substr(0, 1) == string("r")) {
+		cout << "Generate how many random objects? ";
+		cin >> toGenerate;
+		generateRandom(toGenerate);
+	}
+	else cout << "Unrecognized command." << endl;
+	cout << endl << endl;
 }
 
 void generateRandom(unsigned int numGenerate) {
+	vector<double> generated;
 	cout << "This would generate " << numGenerate << " objects."
 		<< endl;
+	insertBestFit(generated);
+	insertFirstFit(generated);
+}
+
+void insertFirstFit(const vector<double> & list) {
+	// TODO
+}
+
+void insertBestFit(const vector<double> & list) {
+	// TODO
 }
